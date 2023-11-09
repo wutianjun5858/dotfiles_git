3a69752dd021b3ac3dc30c0ffdd2a6609c80c515
Jun <tianjun.wu@outlook.com>
2023-11-09 17:52:48 +0800

    fix: update install.sh url

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 
 git clone --bare https://gitee.com/wutianjun5858/jun_dotfile.git $HOME/.dotfiles
 
-sudo curl -o /usr/local/bin/dotfiles_git https://example.com/dotfiles_git
+sudo curl -o /usr/local/bin/dotfiles_git https://raw.githubusercontent.com/wutianjun5858/dotfiles_git/master/dotfiles_git
 
 sudo chmod +x /usr/local/bin/dotfiles_git
 
